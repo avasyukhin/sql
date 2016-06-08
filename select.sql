e1589e0d1171d60c93caa3bcaf38dda833bb8601
avasyukhin <avasyukhin@inbox.ru>
2016-06-08 19:39:45 +0400

    added comments

--- select.sql
+++ select.sql
@@ -1,13 +1,13 @@
 ﻿SELECT pos_name,salary
 FROM POSITIONS
 WHERE CAST (salary AS NUMERIC)>=40000;
-
+-- Зарплаты по должностям
 
 
 SELECT AVG(CAST (salary AS NUMERIC))
 FROM POSITIONS;
 
-
+-- Средняя зарплата
 
 SELECT dep_name, COUNT(emp_id) as employee_qty
 FROM DEPARTMENTS
@@ -15,7 +15,7 @@ INNER JOIN STAFF
 ON dep_id=id
 GROUP BY id;
 
-
+-- Число сотрудников в отделе
 
 SELECT last_name,first_name
 FROM EMPLOYEE
@@ -24,12 +24,16 @@ ON emp_id=id
 WHERE dep_id=1
 ORDER BY last_name,first_name;
 
+-- Сотрудник первого отдела, упорядоченные по фамилии, имени
+
 SELECT dep_name
 FROM DEPARTMENTS
 INNER JOIN STAFF
 ON dep_id=id
 GROUP BY id
 HAVING  COUNT(emp_id)>1;
+
+-- Отделы имеющие более одного сотрудника
 
 
 
