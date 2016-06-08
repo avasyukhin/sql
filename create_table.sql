76180a89be96596cc2dad4cc042041ba1399c22f
avasyukhin <avasyukhin@inbox.ru>
2016-06-08 19:31:08 +0400

    Initial commit

---
+++ create_table.sql
@@ -0,0 +1,62 @@
+﻿CREATE TABLE EMPLOYEE (
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	birth_date DATE NOT NULL 
+);
+CREATE TABLE DEPARTMENTS (
+	id SERIAL PRIMARY KEY,
+	dep_name VARCHAR(50) NOT NULL,
+	open_from TIME  NOT NULL,
+	phone_number CHAR(10) NOT NULL,
+	ip_addr INET
+);
+CREATE TABLE POSITIONS (
+	id SERIAL PRIMARY KEY,
+	pos_name VARCHAR(50) NOT NULL,
+	salary MONEY NOT NULL CHECK (CAST (salary AS NUMERIC)>0)
+);
+CREATE TABLE STAFF (
+	emp_id INTEGER REFERENCES EMPLOYEE (id) NOT NULL,
+	dep_id INTEGER REFERENCES DEPARTMENTS (id) NOT NULL,
+	pos_id INTEGER REFERENCES POSITIONS (id) NOT NULL
+);
+COMMIT;
+
+BEGIN;
+INSERT INTO EMPLOYEE (first_name,last_name,birth_date)
+VALUES ('John','Dow', DATE '1985-07-03'),
+('Joahn','Dowson', DATE '1975-08-01'),
+('Ron','Paul', DATE '1966-07-01'),
+('Alex','Johnson', DATE '1990-11-11'),
+('Simon','Alexander', DATE '1977-12-15');
+COMMIT;
+
+
+
+BEGIN;
+INSERT INTO DEPARTMENTS (dep_name ,open_from ,phone_number ,ip_addr)
+VALUES ('HR', TIME '08:00 ','+556667778',INET '192.168.100.128/25'),
+('R&D', TIME '08:00','+345345678',INET '192.168.4.7/24'),
+('ACCOUNTING', TIME '09:00','+553456788',NULL),
+('MANAGMENT', TIME '09:00','+556444778',NULL),
+('FINANCIAL', TIME '08:00','+552211778',NULL);
+COMMIT;
+
+BEGIN;
+INSERT INTO POSITIONS (pos_name ,salary)
+VALUES ('Chief', '50000'),
+('Secretary', '30000'),
+('Specialist', '40000'),
+('Trainee', '20000'),
+('Support','30000');
+COMMIT;
+
+BEGIN;
+INSERT INTO STAFF (emp_id ,dep_id, pos_id )
+VALUES ('1', '1','1'),
+('2', '1','3'),
+('3', '3','1'),
+('4', '4','1'),
+('5','1','4');
+COMMIT;
---
+++ select.sql
@@ -0,0 +1,37 @@
+﻿SELECT pos_name,salary
+FROM POSITIONS
+WHERE CAST (salary AS NUMERIC)>=40000;
+
+
+
+SELECT AVG(CAST (salary AS NUMERIC))
+FROM POSITIONS;
+
+
+
+SELECT dep_name, COUNT(emp_id) as employee_qty
+FROM DEPARTMENTS
+INNER JOIN STAFF
+ON dep_id=id
+GROUP BY id;
+
+
+
+SELECT last_name,first_name
+FROM EMPLOYEE
+INNER JOIN STAFF
+ON emp_id=id
+WHERE dep_id=1
+ORDER BY last_name,first_name;
+
+SELECT dep_name
+FROM DEPARTMENTS
+INNER JOIN STAFF
+ON dep_id=id
+GROUP BY id
+HAVING  COUNT(emp_id)>1;
+
+
+
+
+
